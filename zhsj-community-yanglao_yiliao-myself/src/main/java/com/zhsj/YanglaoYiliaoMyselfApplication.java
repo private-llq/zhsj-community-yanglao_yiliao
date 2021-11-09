703436007a0f3711c4cca71e8096d4fa78d0ec00
zzm <744698996@qq.com>
2021-11-09 17:40:33 +0800

    添加feign  ---zzm

--- zhsj-community-yanglao_yiliao-myself/src/main/java/com/zhsj/YanglaoYiliaoMyselfApplication.java
+++ zhsj-community-yanglao_yiliao-myself/src/main/java/com/zhsj/YanglaoYiliaoMyselfApplication.java
@@ -3,12 +3,16 @@ package com.zhsj;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author lxjr
  * @date 2021/11/9 10:01
  */
 @SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
 @MapperScan("com.zhsj.community.yanglao_yiliao.myself.mapper")
 public class YanglaoYiliaoMyselfApplication {
     public static void main(String[] args) {
--- zhsj-community-yiliao_service/src/main/java/com/zhsj/YiliaoServiceApplication.java
+++ zhsj-community-yiliao_service/src/main/java/com/zhsj/YiliaoServiceApplication.java
@@ -3,6 +3,8 @@ package com.zhsj;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author lxjr
@@ -10,6 +12,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @MapperScan("com.zhsj.community.yanglao_yiliao.myself.mapper")
+@EnableFeignClients
+@EnableDiscoveryClient
 public class YiliaoServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(YiliaoServiceApplication.class, args);
