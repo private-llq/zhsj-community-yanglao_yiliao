2e701265e5c7cdc785c09791b3c08b758abf9a7b
liu-jr <1826489162@qq.com>
2021-11-10 09:17:42 +0800

    增加实例代码

--- zhsj-community-healthdata/src/main/java/com/zhsj/community/yanglao_yiliao/healthydata/controller/TestController.java
+++ zhsj-community-healthdata/src/main/java/com/zhsj/community/yanglao_yiliao/healthydata/controller/TestController.java
@@ -3,6 +3,8 @@ package com.zhsj.community.yanglao_yiliao.healthydata.controller;
 import com.zhsj.basecommon.enums.ErrorEnum;
 import com.zhsj.basecommon.exception.BaseException;
 import com.zhsj.basecommon.vo.R;
+import com.zhsj.baseweb.support.ContextHolder;
+import com.zhsj.baseweb.support.LoginUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +20,7 @@ public class TestController {
 
     @GetMapping("/test")
     public R<Void> test() {
+        LoginUser loginUser = ContextHolder.getContext().getLoginUser();
         throw new BaseException(ErrorEnum.SERVER_BUSY);
     }
 
