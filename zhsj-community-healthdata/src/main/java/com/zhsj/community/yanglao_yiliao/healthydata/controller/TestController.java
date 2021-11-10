c3b9d2405aae7ea6ec9f5fe018b489241f0870bf
liu-jr <1826489162@qq.com>
2021-11-10 09:16:03 +0800

    增加实例代码

---
+++ zhsj-community-healthdata/src/main/java/com/zhsj/community/yanglao_yiliao/healthydata/controller/TestController.java
@@ -0,0 +1,28 @@
+package com.zhsj.community.yanglao_yiliao.healthydata.controller;
+
+import com.zhsj.basecommon.enums.ErrorEnum;
+import com.zhsj.basecommon.exception.BaseException;
+import com.zhsj.basecommon.vo.R;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 实例代码
+ * @author lxjr
+ * @date 2021/11/10 9:14
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/test")
+    public R<Void> test() {
+        throw new BaseException(ErrorEnum.SERVER_BUSY);
+    }
+
+    @GetMapping("/test1")
+    public R<Object> test1() {
+        return R.ok(new Object());
+    }
+}
